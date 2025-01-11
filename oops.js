e95ff408fb793eef1016d18c93284289d614953e
Suhailsubair007 <clashersuhail221@gmail.com>
2025-01-11 11:58:10 +0530

    OPP concepts added

--- oops.js
+++ oops.js
@@ -62,3 +62,123 @@ var Dog = /** @class */ (function () {
 var mydog = new Dog('Buddyyy');
 console.log(mydog.name);
 mydog.makeSound();
+var Animal = /** @class */ (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    return Animal;
+}());
+// A Bird class extends Animal and implements Flyable
+var Bird = /** @class */ (function (_super) {
+    __extends(Bird, _super);
+    function Bird(name) {
+        return _super.call(this, name) || this;
+    }
+    Bird.prototype.fly = function () {
+        console.log("".concat(this.name, " is flying!"));
+    };
+    return Bird;
+}(Animal));
+var myBird = new Bird("Parrot");
+myBird.fly(); // Output: Parrot is flying!
+console.log(myBird.name); // Output: Parrot
+// Access Modifiers================================================================
+var Person = /** @class */ (function () {
+    function Person(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    Person.prototype.getName = function () {
+        return this.name;
+    };
+    Person.prototype.getAge = function () {
+        return this.age;
+    };
+    return Person;
+}());
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(name, age, gender, employeeId) {
+        var _this = _super.call(this, name, age, gender) || this;
+        _this.employeeId = employeeId;
+        return _this;
+    }
+    Employee.prototype.getEmployeeDetails = function () {
+        console.log("Name: ".concat(this.getName(), ", Age: ").concat(this.getAge(), ", Gender: ").concat(this.gender, ", Employee ID: ").concat(this.employeeId));
+    };
+    return Employee;
+}(Person));
+var emp = new Employee("Alice", 30, "Female", 12345);
+emp.getEmployeeDetails();
+console.log(emp.gender); // Accessible
+// console.log(emp.age); // Error: Property 'age' is protected
+// console.log(emp.name); // Error: Property 'name' is private
+//Implemets and Extens combined example=============================================
+var Classmate = /** @class */ (function () {
+    function Classmate() {
+    }
+    Classmate.prototype.bookName = function () {
+        console.log("Hi, I am book 1");
+    };
+    return Classmate;
+}());
+var Book = /** @class */ (function (_super) {
+    __extends(Book, _super);
+    function Book(page) {
+        var _this = _super.call(this) || this;
+        _this.page = page;
+        return _this;
+    }
+    Book.prototype.displayPage = function () {
+        console.log("This book has ".concat(this.page, " pages."));
+    };
+    return Book;
+}(Classmate));
+var myBook = new Book(200);
+myBook.bookName(); // Output: Hi, I am book 1
+myBook.displayPage(); // Output: This book has 200 pages.
+// =========================================================================================
+var Parent = /** @class */ (function () {
+    function Parent() {
+    }
+    Parent.prototype.greet = function () {
+        console.log("Hello from Parent");
+    };
+    return Parent;
+}());
+var SuperHero = /** @class */ (function (_super) {
+    __extends(SuperHero, _super);
+    function SuperHero() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    SuperHero.prototype.fly = function () {
+        console.log("Flying!");
+    };
+    return SuperHero;
+}(Parent));
+var hero = new SuperHero();
+hero.greet(); // Output: Hello from Parent
+hero.fly(); // Output: Flying!
+//======================================OVERRIDING=====================================
+var A = /** @class */ (function () {
+    function A() {
+    }
+    A.prototype.print = function () {
+        console.log('I am class A');
+    };
+    return A;
+}());
+var B = /** @class */ (function (_super) {
+    __extends(B, _super);
+    function B() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    B.prototype.print = function () {
+        _super.prototype.print.call(this);
+        console.log('I am class B');
+    };
+    return B;
+}(A));
+var z = new B();
+z.print();
--- oops.ts
+++ oops.ts
@@ -65,31 +65,31 @@ mydog.makeSound();
 
 interface Flyable {
     fly(): void;
-  }
-  
-  class Animal {
+}
+
+class Animal {
     name: string;
-  
+
     constructor(name: string) {
-      this.name = name;
+        this.name = name;
     }
-  }
-  
-  // A Bird class extends Animal and implements Flyable
-  class Bird extends Animal implements Flyable {
+}
+
+// A Bird class extends Animal and implements Flyable
+class Bird extends Animal implements Flyable {
     constructor(name: string) {
-      super(name);
+        super(name);
     }
-  
+
     fly(): void {
-      console.log(`${this.name} is flying!`);
+        console.log(`${this.name} is flying!`);
     }
-  }
-  
-  const myBird = new Bird("Parrot");
-  myBird.fly();            // Output: Parrot is flying!
-  console.log(myBird.name); // Output: Parrot
-  
+}
+
+const myBird = new Bird("Parrot");
+myBird.fly();            // Output: Parrot is flying!
+console.log(myBird.name); // Output: Parrot
+
 // Access Modifiers================================================================
 
 class Person {
@@ -130,3 +130,72 @@ emp.getEmployeeDetails();
 console.log(emp.gender); // Accessible
 // console.log(emp.age); // Error: Property 'age' is protected
 // console.log(emp.name); // Error: Property 'name' is private
+
+
+//Implemets and Extens combined example=============================================
+
+class Classmate {
+    bookName() {
+        console.log("Hi, I am book 1");
+    }
+}
+
+interface Book_interface {
+    page: number;
+}
+
+class Book extends Classmate implements Book_interface {
+    page: number;
+
+    constructor(page: number) {
+        super();
+        this.page = page;
+    }
+
+    displayPage() {
+        console.log(`This book has ${this.page} pages.`);
+    }
+}
+
+const myBook = new Book(200);
+myBook.bookName(); // Output: Hi, I am book 1
+myBook.displayPage(); // Output: This book has 200 pages.
+
+// =========================================================================================
+
+class Parent {
+    greet() {
+        console.log("Hello from Parent");
+    }
+}
+
+interface CanFly {
+    fly(): void;
+}
+
+class SuperHero extends Parent implements CanFly {
+    fly() {
+        console.log("Flying!");
+    }
+}
+
+const hero = new SuperHero();
+hero.greet(); // Output: Hello from Parent
+hero.fly();   // Output: Flying!
+
+
+//======================================OVERRIDING=====================================
+class A {
+    print() {
+        console.log('I am class A');
+    }
+}
+class B extends A {
+    print() {
+        super.print()
+        console.log('I am class B');
+    }
+}
+
+const z = new B();
+z.print() 
